5ac39eda4b31ef3855927fce2b2d9d4bf0ec4cb9
Do-Kim93 <ehhgus200293@gmail.com>
2025-06-11 13:52:18 +0900

    커밋 테스트

---
+++ src/main/java/com/koreait/exam/chat_25_05/Chat2505Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_25_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat2505Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chat2505Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/exam/chat_25_05/Chat2505ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_25_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat2505ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
